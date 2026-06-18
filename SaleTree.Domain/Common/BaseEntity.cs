a8597b7b04fe34af6db57b8bcd32b7157be2a28b
Ali Ahsan <aliahsan8751@gmail.com>
2026-06-18 15:58:11 +0500

    SAL-4 added basic entities in domani

---
+++ SaleTree.Domain/Common/BaseEntity.cs
@@ -0,0 +1,6 @@
+namespace SaleTree.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+}
---
+++ SaleTree.Domain/Constants/Roles.cs
@@ -0,0 +1,10 @@
+namespace SaleTree.Domain.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Seller = "Seller";
+        public const string Customer = "Customer";
+
+    }
+}
